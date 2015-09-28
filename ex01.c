78e419e6e366132f6d83b140ca45ec1f410eefc3
Kristian Andersson <krisan13@uia.no>
2015-09-28 10:56:13 +0200

    Open file script KA

---
+++ ex01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+int fd;
+
+fd = open("madagascar", O_WRONLY | O_TRUNC);
+
+if (fd == -1){
+printf("Error opening file\n");
+}else{
+//printf(fd);
+printf("File opened\n");
+
+}
+
+
+return 0;
+}
