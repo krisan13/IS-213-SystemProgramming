66ae40367bee782c81fe7169dd088418fda84ad8
Gerrit <gerrit.seger@gmail.com>
2015-10-05 11:06:41 +0200

    Add signal interrupt handler and writing to file

--- append.c
+++ append.c
@@ -4,6 +4,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+
+static volatile bool keepRunning = true;
+
+static void interrupt(int signo)
+{
+  printf("Interrupted\n");
+  keepRunning = false;
+}
 
 int main(int argc, char **argv)
 {
@@ -23,5 +33,17 @@ int main(int argc, char **argv)
     return -1;
   }
 
+  signal(SIGINT, interrupt);
+  while (keepRunning)
+  {
+    if (write(fd, argv[2], sizeof(argv[2])) == -1)
+    {
+      perror("write");
+      break;
+    }
+    sleep(1);
+  }
+
+  close(fd);
   return 0;
 }
