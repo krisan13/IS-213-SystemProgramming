8f81ecf0c0b4c52879f0a7191c134e2e48e35d58
Gerrit <gerrit.seger@gmail.com>
2015-10-05 23:14:22 +0200

    Replace strlen by sizeof

--- append.c
+++ append.c
@@ -59,7 +59,7 @@ int main(int argc, char **argv)
   signal(SIGINT, interrupt);
   while (keepRunning)
   {
-    if (write(fd, buffer, strlen(buffer)) == -1)
+    if (write(fd, buffer, sizeof(buffer)) == -1)
     {
       perror("write");
       break;
